2e370372f096ab91e03607d54d470326e757ce46
shashi-1992 <90611979+shashi-1992@users.noreply.github.com>
2025-11-17 23:56:22 +0530

    #5Fix formatting in Weather-Forecast.go

--- Weather-Forecast.go
+++ Weather-Forecast.go
@@ -12,3 +12,4 @@ func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
 }
+
