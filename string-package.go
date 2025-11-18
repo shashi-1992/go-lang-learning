dc82eb9e3720bbcf05d617f4b85bc78e7c3f1838
shashi-1992 <90611979+shashi-1992@users.noreply.github.com>
2025-11-18 23:11:39 +0530

    Document strings package functions
    Added documentation for the strings package functions.

--- string-package.go
+++ string-package.go
@@ -3,3 +3,22 @@ The strings package contains many useful functions to work on strings.
 import "strings"
 
 strings.ToUpper("test") // => "TEST"
+=============================================================
+About Strings Package
+The strings package contains many useful functions to work on strings:
+
+Function	Purpose
+ToLower	Convert the string to lower case
+ToUpper	Convert the string to upper case
+TrimSpace	Trim leading and trailing whitespace
+Index	Find the index of the first instance of a substring within a string
+Replace	Replace one occurrence of a substring in a string
+ReplaceAll	Replace all occurrences of a substring in a string
+Split	Split a string into parts based on a separator
+HasSuffix	Check if a string ends with a specific substring
+Count	Count the number of occurrences of a substring within a string
+import "strings"
+
+strings.ToLower("Gopher")    // => "gopher"
+strings.Index("Apple", "le") // => 3
+strings.Count("test", "t")   // => 2
