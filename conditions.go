a5c434834c6d9caeac1ddcd7a9766462fd503ce3
shashi-1992 <90611979+shashi-1992@users.noreply.github.com>
2025-11-19 22:23:24 +0530

    Add documentation for conditionals in Go
    Added documentation on conditionals in Go, including examples of if, else if, and else statements.

---
+++ conditions.go
@@ -0,0 +1,31 @@
+About Conditionals If
+Conditionals in Go are similar to conditionals in other languages. The underlying type of any conditional operation is the bool type, which can have the value of true or false. Conditionals are often used as flow control mechanisms to check for various conditions.
+
+For checking a particular case an if statement can be used, which executes its code if the underlying condition is true like this:
+
+var value string
+
+if value == "val" {
+    return "was val"
+}
+In scenarios involving more than one case many if statements can be chained together using the else if and else statements.
+
+var number int
+result := "This number is "
+
+if number > 0 {
+   result += "positive"
+} else if number < 0 {
+    result += "negative"
+} else {
+   result += "zero"
+}
+If statements can also include a short initialization statement that can be used to initialize one or more variables for the if statement. For example:
+
+num := 7
+if v := 2 * num; v > 10 {
+    fmt.Println(v)
+} else {
+    fmt.Println(num)
+}
+// Output: 14
